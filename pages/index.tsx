c5e28c35bd9637207062aede786c2e93c98453a4
leon <leonlenk@gmail.com>
2023-12-18 16:40:03 -0800

    Added gradient background and text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import starStyles from "../styles/stars.module.scss";
 import footerStyles from "../styles/footer.module.scss";
-import Head from "next/head";
+import indexStyles from "../styles/index.module.scss";
 
 export default function Page() {
     return (
     <>
-    <Head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-    </Head>
     <main>
         <div id={starStyles.background}/>
         <div id={starStyles.midground}/>
@@ -15,11 +12,11 @@ export default function Page() {
         <div id={footerStyles.footer}>
             <div id={footerStyles.iconContainer}>
                 <a href="mailto:leonlenk@gmail.com" className="fa fa-google" id={footerStyles.socialIcon} />
-                <a href="https://github.com/leonlenk" className="fa fa-github" id={footerStyles.socialIcon} />
                 <a href="https://www.linkedin.com/in/leon-lenk/" className="fa fa-linkedin" id={footerStyles.socialIcon} />
+                <a href="https://github.com/leonlenk" className="fa fa-github" id={footerStyles.socialIcon} />
             </div>
         </div>
-        <h1>Hello, Next.js!</h1>
+        <h1 id={indexStyles.title}>Welcome To My Website!</h1>
     </main>
     </> 
     );
