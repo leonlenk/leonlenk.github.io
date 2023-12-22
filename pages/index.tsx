9e5e1337ee406ee841795cb2890ec4fc45bde0dd
leon <leonlenk@gmail.com>
2023-12-22 10:25:15 -0800

    Refactor
    Changed id to className so elements could be given unique id

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,26 +3,32 @@ import footerStyles from "../styles/footer.module.scss";
 import indexStyles from "../styles/index.module.scss";
 
 export default function Page() {
+
     return (
     <>
     <main>
-        <div id={starStyles.starContainer}>
-            <p id={starStyles.background}>★</p>
-            <p id={starStyles.midground}>★</p>
-            <p id={starStyles.foreground}>★</p>
+        <div className={starStyles.starContainer}>
+            <p className={starStyles.background}>★</p>
+            <p className={starStyles.midground}>★</p>
+            <p className={starStyles.foreground}>★</p>
         </div>
-        <div id={footerStyles.footer}>
-            <img src="mountains_footer.svg" id={footerStyles.footerImage}/>
-            <div id={footerStyles.iconContainer}>
+        <div className={footerStyles.footer}>
+            <object type="image/svg+xml" data="mountains_footer.svg" className={footerStyles.footerImage}/>
+            <div className={footerStyles.iconContainer}>
                 <a href="mailto:leonlenk@gmail.com" className="fa fa-google" id={footerStyles.socialIcon} />
                 <a href="https://www.linkedin.com/in/leon-lenk/" className="fa fa-linkedin" id={footerStyles.socialIcon} />
                 <a href="https://github.com/leonlenk" className="fa fa-github" id={footerStyles.socialIcon} />
             </div>
         </div>
-        <div id={indexStyles.scrollableContainer}>
-            <h1 id={indexStyles.title}>Welcome To My Website!</h1>
-            {/* <img src="whitePawn.svg" id={indexStyles.pawn} /> */}
-        </div> 
+        <div className={indexStyles.scrollableContainer}>
+            <h1 className={indexStyles.title}>Welcome To My Website!</h1>
+            <div className={indexStyles.constellationContainer}> 
+                <div className={indexStyles.pawnContainer}>
+                    {/* <object type="image/svg+xml" data="whitePawn.svg" className={indexStyles.whitePawn} />
+                    <object type="image/svg+xml" data="star.svg" className={indexStyles.pawnStars} /> */}
+                </div>
+            </div>
+        </div>
     </main>
     </> 
     );
