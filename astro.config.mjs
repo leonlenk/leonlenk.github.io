2dc60f2fc850729e3aac93524f5bb50ca7773ad9
leon <leonlenk@gmail.com>
2025-08-25 16:31:25 -0500

    update SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,18 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.leonlenk.com",
+  build: {
+    assets: "assets",
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['astro'],
+          },
+        },
+      },
+    },
+  },
 });
